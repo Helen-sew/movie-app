1a4be055b1d92b36c0fd2e696d9d68a2d1a7b49f
Helen-sew <53653038+Helen-sew@users.noreply.github.com>
2020-07-15 22:38:23 +0800

    Updates

---
+++ precache-manifest.d6593e2d2a692f4ec03373425801c734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23353530a5fcc2876ae0d5b7bf238dcf",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "1db208b37d0fdefee830",
+    "url": "/movie-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56958c5260bbefbface8",
+    "url": "/movie-app/static/js/2.dd277096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.dd277096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db208b37d0fdefee830",
+    "url": "/movie-app/static/js/main.2d1f5531.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
